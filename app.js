e1d3131688f84706f7b6aa31800f2e3c9c308f39
krupa140699 <krupabhatt14061999@gmail.com>
2023-07-24 14:32:25 +0530

    add connection string

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express');
-
+const mongoose  = require('mongoose');
+const DB = 'mongodb+srv://krupabhatt14061999:Krupa%401406@cluster0.5lbqkpo.mongodb.net/'
 const app = express();
+
+const connectionParams={
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true 
+}
+
+mongoose.connect(DB).then(() =>{
+  console.log('connection successfull')
+}).catch((err)=>console.log('no connection'))
 require('./routes')(app);
 
   app.listen(4000);
